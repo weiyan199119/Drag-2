b1f40b8c745f293364e70a6d1c4ab58c86dbce16
wikai7777777 <1622186950@qq.com>
2017-05-21 17:49:24 +0800

    banben2

--- drag.js
+++ drag.js
@@ -2,7 +2,11 @@
  	var obj = document.getElementById(id);
  	var disx = 0;
  	var dixY = 0;
- 	obj.onmousedown = function(){
+ 	obj.onmousedown = function(ev){
+ 		var ev = ev || ev.event;
+
+ 		disX = ev.pageX-obj.offsetLeft;
+ 		disY = ev.pageY-obj.offsetTop;
 
  	}
 
