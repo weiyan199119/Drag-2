129255742addccd0a689ced28e6ca0a4baeae54f
wikai7777777 <1622186950@qq.com>
2017-05-21 18:30:33 +0800

    c9

--- drag.js
+++ drag.js
@@ -7,6 +7,11 @@
  		disX = ev.pageX-obj.offsetLeft;
  		disY = ev.pageY-obj.offsetTop;
 
+ 		document.onmousemove = function(ev){
+ 			obj.style.left = ev.pageX-disX+'px';
+ 			obj.style.top = ev.pageY-disY+'px';
+ 		}
+
  	}
 
  }
